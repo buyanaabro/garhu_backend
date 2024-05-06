ea5d44a04d7016589270a2da9b6ad78e15aa5e15
buyanaabro <buunaa.n@gmail.com>
2024-05-06 16:49:25 +0800

    adjusted tsconfig.js, reverted hash-password.ts

--- src/utils/hash-password.ts
+++ src/utils/hash-password.ts
@@ -1,26 +1,14 @@
+// Node imports
 import crypto from "crypto";
-import secrets from "../configs/secrets.json";
+
+// Config import
+import { sha256_secret } from "../configs/secrets.json";
 
 function hashPassword(password: string): string {
   return crypto
-    .createHmac("sha256", secrets.sha256_secret)
+    .createHmac("sha256", sha256_secret)
     .update(password)
     .digest("base64");
 }
 
 export default hashPassword;
-
-// Node imports
-// import crypto from "crypto";
-
-// // Config import
-// import { sha256_secret } from "../configs/secrets.json";
-
-// function hashPassword(password: string): string {
-//   return crypto
-//     .createHmac("sha256", sha256_secret)
-//     .update(password)
-//     .digest("base64");
-// }
-
-// export default hashPassword;
