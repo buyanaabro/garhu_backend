d4251f36e44b48f0e0c99b9eb0feee4f230be128
buyanaabro <buunaa.n@gmail.com>
2024-05-06 16:40:49 +0800

    adjusted hash-password.ts

--- src/utils/hash-password.ts
+++ src/utils/hash-password.ts
@@ -1,12 +1,26 @@
-// Node imports
-import crypto from "crypto"
-
-// Config import
-import { sha256_secret } from "../configs/secrets.json";
-
+import crypto from "crypto";
+import secrets from "../configs/secrets.json";
 
 function hashPassword(password: string): string {
-    return crypto.createHmac("sha256", sha256_secret).update(password).digest("base64");
+  return crypto
+    .createHmac("sha256", secrets.sha256_secret)
+    .update(password)
+    .digest("base64");
 }
 
 export default hashPassword;
+
+// Node imports
+// import crypto from "crypto";
+
+// // Config import
+// import { sha256_secret } from "../configs/secrets.json";
+
+// function hashPassword(password: string): string {
+//   return crypto
+//     .createHmac("sha256", sha256_secret)
+//     .update(password)
+//     .digest("base64");
+// }
+
+// export default hashPassword;
